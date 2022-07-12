adaaf13d0af6f25cccdf4024bdbcf770b5e2c40e
Tom <tmbdev@gmail.com>
2022-07-12 16:44:09 -0700

    added shell.nix for golang

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    go_1_18
+    gotools
+    gopls
+    # go-outline
+    # gocode
+    # gopkgs
+    # gocode-gomod
+    # godef
+    # golint
+  ];
+}
